a5df13d64ec27fb280c665afb140f1153c52773a
Ivy Hwa <104978202+Ivy-09@users.noreply.github.com>
2023-01-02 17:44:31 +0000

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,47 +1,178 @@
-// form close and open
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-}
+<!DOCTYPE html>
+<html> 
 
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-}
+    <!--Head-->
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width" />
+        <title>Tech Academy Portfolio</title>
+        <link rel="stylesheet" type="text/css" href="./portfolio.css">
+        <script src="js/portfolio.js"></script>
+    </head>
 
-// This function displays the first image in the slideshow when the page loads
-var slideIndex = 1;
-showSlides(slideIndex);
+    <!--Body-->
+    <body class="Background">
+        
+        <!--Navbar-->
+        <div class="Navbar">
+            <a class="active" href="#Home">Home</a>
+            <a href="#About">About</a>
+            <a href="#GitHub">GitHub</a>
+            <a href="#Contact">Contact</a>
+        </div>
 
-// Slide changes by clicking on arrows
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+        <!--Breaks inserted to add space between navbar and the first heading-->
+        
+        <br>
+        <br>
+        <!--Background video-->
+        <div class="video-wrapper">
+            <video playsinline autoplay muted loop id="background.video" >
+                <source src="./background.webm" type="video/webm">
+                <source src="./background.mp4" type="video/mp4">
+                <!--Display message if video fails to run-->
+                Your browser does not support the video tag.   
+            </video>
+            
+        </div>
+    
+            
+        <!--Video text-->
+        <div class="Overlay">
+            <h1> Ivy's website design wonderland</h1>
+            <br> 
+            </div>
+            <strong>
+                <p class="center">
+                    <br>  I am Ivy. Welcome to my portfolio website. All my work will be displayed in here!
+                    
+                    <br> Hope you'll enjoy it! 
+                </p>
+            </strong>
+        </div>
 
-// Slide changes by clicking on dots
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+
+         <!-- Slideshow section added -->
+         <div id="Slideshow">
+            <div id="Slideshow_Background"> 
+                <div id="Slideshow_Container">
+                    
+                    <!-- Slide 1 of 4 -->
+                    <div class="mySlides fade">
+                        <img class="Slideshow_Images" src="image/html.png" alt="HTML logos"> 
+                        <div class="text">HTML</div>
+                    </div>
+                    <!-- Slide 2 of 4 -->
+                    <div class="mySlides fade">
+                        <img class="Slideshow_Images" src="image/Self.jpg" alt="Picture of yourself">
+                        <div class="text">I am a newbie in software developement.Here is a list of languages I've learnt.</div>
+                    </div>
+                    <!-- Slide 3 of 4 -->
+                    <div class="mySlides fade">
+                        <img class="Slideshow_Images" src="image/js.png" alt="JS logo">
+                        <div class="text">JavaScript</div>
+                    </div>
+                    <!-- Slide 4 of 4 -->
+                    <div class="mySlides fade">
+                        <img class="Slideshow_Images" src="image/css.png" alt="CSS logo">
+                        <div class="text">CSS  </div>
+                    </div>
+                    
+                    <!-- Previous and next buttons -->
+                    <a class="Previous" onclick="plusSlides(-1)">&#10094;</a>
+                    <a class="Next" onclick="plusSlides(1)">&#10095;</a>
+                </div>
+                <br> <!-- Adds a space between the slideshow images and the dots -->
+
+                <!-- Slideshow dots (these are another way for users to navigate through the slideshow) -->
+                <div style="text-align: center">
+                    <span class="dot" onclick="currentSlide(1)"></span>
+                    <span class="dot" onclick="currentSlide(2)"></span>
+                    <span class="dot" onclick="currentSlide(3)"></span> 
+                    <span class="dot" onclick="currentSlide(4)"></span> 
+                </div>
+            </div>
+        </div>
 
 
-function showSlides(n) {
-    var slides = document.getElementsByClassName("mySlides"); // This takes all elements with the class name "mySlides" and stores them in the variable array "slides"
-    var dots = document.getElementsByClassName("dot"); // This takes all elements with the class name "dot" and stores them in the variable array "dots"
-    if (n > slides.length) {slideIndex = 1}; // If n (the number passed into the function) is greater than the length of the array "slides", the slideIndex is set to 1
-    if (n < 1) {slideIndex = slides.length}; // If n (the number passed into the function) is less than 1, te slideIndex is set to the length of the array "slides"
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; // This for loop takes each item in the array "slides" and sets the display to none
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", ""); // This for loop takes each item in the array "dots" and removes "active" which removes the active styling
-    }
-    slides[slideIndex - 1].style.display = "block"; // This displays the image in the slideshow
-    dots[slideIndex - 1].className += " active"; // This adds the active styling to the dot associated with the image
-}
+        <!--About section-->
+        <div class="Row" id="About">
+            <!--Left column-->
+            <div class="Column_2">
+                <img src="./street.jpg" alt="Asian street">
+            </div>
+            <!--Right column-->
+            <div class="Column_1">
+                <h1>About</h1>
+                <p>
+                    I am a newbie in website designing! 
+                    <br>
+                    <br>I am a graduate of <a href="https://www.learncodinganywhere.com" target="_blank">The Tech Academy</a>’s Software Developer Boot Camp, I am trained and experienced in the the following web and programming languages: HTML, CSS, and JavaScrip. 
+                    <br>Contact me if you would like to get in touch. <a href="#Contact">Contact</a> me below!
+                </p>
+            </div>
+         
 
-// This code will create close the contact form when the user clicks off of it
-// The first step is to add an event listener for any clicks on the website
-document.addEventListener("click", function(event){
-    // Here we state that if the click happens on the cancel button OR anywhere that is not the contact form AND the click does not happen on any element with the contact class then call the closeForm() function
-    if (event.target.matches(".cancel") || !event.target.closest(".form-popup") && !event.target.closest(".Pop_Up_Button") && !event.target.closest(".contact")){
-        closeForm()
-    }
-}, false )
+        <!--GitHub section-->
+        <div class="Row" id="GitHub">
+            <!--Left column-->
+            <div class="Column_1">
+                <h1>GitHub</h1>
+                <p>
+                    My coding project is available on Github here and more projects will be listed on my portfolio:
+                    <br>
+                    <p class="center"><a href="https://github.com/Ivy-09" target="_blank">My Github</a></p>
+                </p>
+            </div>
+            <!--Right column-->
+            <div class="Column_2">
+                <b><p>Here is a list of projects I did</p> </b>
+                <li>One Page Website</li>
+                <p>The one page displayed how to link website to video and pictures along with form card. </p>
+                <li>Fun Animal world</li>
+                <p>The site is a site featuring cute animals I like and there are different pages with video and ophotos are linked via nav bar</p>
+                <li>Vintage Shop Site</li>
+                <p>The site is customized for my vintage shop. 
+                It is created with Boostrap 4. It is composed of navbar,jumbotron, formcard and CSS for styling.The characteristic of each vintage items is marked with different tag such as the meterial and colors.</p>
+               
+
+            </div>
+        
+        </div>
+
+        <!--Contact section-->
+        <div class="Row" id="Contact">
+            <!--Contact image, left column-->
+            <div class="Column_2 Column_tall">
+                <img src="./thumbnail_2.jpg" alt="Contact_Image">
+            </div>
+        </div>
+           <!-- Contact form -->
+        <div id="Contact">
+            <button onclick="openForm()" class="Pop_Up_Button">CONTACT</button>
+            <div class="form-popup" id="myForm">
+                 <form action="https://formspree.io/info@learncodinganywhere.com" method="POST" class="form-container"> <!-- Here we are using a free online service from https://formspree.io/ to send our contact form to an email address of our choosing - note: it will not run unless live on the internet, you have to push it to your online website first -->
+                     <h1>Contact</h1>
+                     <label for="name"><b>Name</b></label>
+                       <input type="text" placeholder="Type your name here" name="name" required>
+                     <label for="phone"><b>Phone</b></label>
+                       <input type="text" placeholder="Type your phone number here" name="phone">
+                     <label for="email"><b>Email</b></label>		
+                       <input type="text" placeholder="Type your email address here" name="email" required>
+                     <label for="message"><b>Message</b></label>
+                       <input type="text" placeholder="Type your message here" name="message">
+          
+                     <button type="submit" class="btn">SUBMIT</button>
+                     <button type="button" class="btn cancel" onclick="closeForm()">CLOSE</button>
+                 </form>
+            </div>
+         </div>
+
+        <!--Footer section-->
+        <footer>
+            <p>
+                <p class="center">&copy Ivy Hwa, <a href="https://www.learncodinganywhere.com/" target="_blank">The Tech Academy</a></p>
+                <br>
+            </p>
+        </footer>
+    </body>
